9862411f9d3cb573c583ab3887b28fa2d7a64281
HelloWorld7894 <danpojhan@protonmail.com>
2024-05-01 17:24:05 +0200

    HOTFIX: fixed fonts lol

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ rm -rf ./package
 mkdir ./package
 sleep 2
 
-pyinstaller --onefile --windowed --name=flytest --specpath ./package/spec --distpath ./package/dist --workpath ./package/build ./src/main.py
+pyinstaller --onefile --windowed --name=flytest --add-data ../../src/fonts:./fonts/ --specpath ./package/spec --distpath ./package/dist --workpath ./package/build ./src/main.py
