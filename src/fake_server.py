4640c194fc8cb1e71cd760f54790e9bb5d6c387d
HelloWorld7894 <danpojhan@protonmail.com>
2024-05-29 19:15:01 +0200

    testing server doesnt crash

--- src/fake_server.py
+++ src/fake_server.py
@@ -1,26 +1,18 @@
-#
-# Fake server for app testing
-#
-
 import socket
+import threading
 import numpy as np
 import time
 
 HOST = "127.0.0.1"
 PORT = 7500
-#client_sockets = []
 
-def run_server(logger, verbose = False):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        client_socket, addr = s.accept()
-        client_socket.settimeout(1)
-        #client_sockets.append(client_socket) TODO
-        client_socket
-        with client_socket:
-            print(f"Connected by {addr}")
-            while True:
+# Function to handle client connections
+def handle_client(conn, addr, logger, verbose):
+    print(f"Connected by {addr}")
+    try:
+        while True:
+            try:
+                #sending data
                 y_accel = np.random.randint(0, 255) + round(np.random.random(), 2)
                 y_gyro = np.random.randint(0, 255) + round(np.random.random(), 2)
                 y_PID = np.random.randint(0, 255) + round(np.random.random(), 2)
@@ -30,15 +22,37 @@ def run_server(logger, verbose = False):
                     logger.log("Server", f"Set Y values of: {y_accel}, {y_gyro}, {y_PID}")
 
                 data = f"{y_accel},{y_gyro},{y_PID}\n"
-                client_socket.send(data.encode())
+                conn.send(data.encode())
 
+                #getting data
                 try:
-                    recv_data = client_socket.recv(1024).decode("utf-8")
+                    recv_data = conn.recv(1024).decode("utf-8")
                 except TimeoutError:
                     continue #timeout
 
                 if verbose:
                     logger.log("Server", f"received data: {recv_data}")
+            except ConnectionResetError:
+                print("reset")
+                break
+                
+    except ConnectionResetError and BrokenPipeError:
+        conn.close()
+        print(f"Connection closed by {addr}")
+
+# Main function to start the server
+def run_server(logger, verbose = False):
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+
+        print(f"Server listening on {HOST}:{PORT}")
+        while True:
+            conn, addr = s.accept()
+            conn.settimeout(1)
+            client_thread = threading.Thread(target=handle_client, args=(conn, addr, logger, verbose))
+            client_thread.start()
 
 if __name__ == "__main__":
     run_server()
