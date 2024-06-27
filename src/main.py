db88d6d0b6e7823c1bb124b7074570b0d47d9bd0
HelloWorld7894 <danpojhan@protonmail.com>
2024-06-27 17:31:19 +0200

    convert to PEP8

--- src/main.py
+++ src/main.py
@@ -67,6 +67,7 @@ class MainApp:
     def gui_init(self):
         self.program_path = Path(__file__)
         self.bold_font_path = os.path.join(str(self.program_path.parent), "fonts/OpenSansBold-8wJJ.ttf")
+
         self.default_font_path = os.path.join(str(self.program_path.parent), "fonts/OpenSansSemibold-wO7w.ttf")
 
         dpg.create_context()
@@ -74,7 +75,9 @@ class MainApp:
             self.header_font = dpg.add_font(self.bold_font_path, 30 * 2)
             self.med_header_font = dpg.add_font(self.bold_font_path, 20 * 2)
             self.default_font = dpg.add_font(self.default_font_path, 15 * 2)
-        dpg.set_global_font_scale(0.5) # temp fix for: https://github.com/hoffstadt/DearPyGui/issues/1380
+
+        # temp fix for: https://github.com/hoffstadt/DearPyGui/issues/1380
+        dpg.set_global_font_scale(0.5)
 
         with dpg.window(label='Data', tag='window'):
             with dpg.group(horizontal=True):
@@ -83,74 +86,105 @@ class MainApp:
                     dpg.add_plot_legend()
 
                     # REQUIRED: create x and y axes, set to auto scale.
-                    x_axis = dpg.add_plot_axis(dpg.mvXAxis, label='x', tag='x_axis_x')
-                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label='y', tag='y_axis_x')
-
+                    x_axis = dpg.add_plot_axis(dpg.mvXAxis,
+                                               label='x',
+                                               tag='x_axis_x')
+                    y_axis = dpg.add_plot_axis(dpg.mvYAxis,
+                                               label='y',
+                                               tag='y_axis_x')
 
                     # series belong to a y axis. Note the tag name is used in the update
                     # function update_data
-                    dpg.add_line_series(x=list(self.data_x),y=list(self.accel_data_x), 
-                                        label='Accel data', parent='y_axis_x', 
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.accel_data_x),
+                                        label='Accel data',
+                                        parent='y_axis_x',
                                         tag='accel_x')
-                    
-                    dpg.add_line_series(x=list(self.data_x),y=list(self.gyro_data_x), 
-                                        label='Gyro data', parent='y_axis_x', 
+
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.gyro_data_x),
+                                        label='Gyro data',
+                                        parent='y_axis_x',
                                         tag='gyro_x')
-                    
-                    dpg.add_line_series(x=list(self.data_x),y=list(self.PID_data_x), 
-                                        label='PID data', parent='y_axis_x', 
+
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.PID_data_x),
+                                        label='PID data',
+                                        parent='y_axis_x',
                                         tag='pid_x')
 
-                    dpg.add_line_series(x=list(self.data_x), y=list(self.setpoint_x),
-                                        label='Setpoint', parent='y_axis_x',
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.setpoint_x),
+                                        label='Setpoint',
+                                        parent='y_axis_x',
                                         tag='setpoint_x')
-                    
+
                 with dpg.plot(label='PID Y'):
                     # optionally create legend
                     dpg.add_plot_legend()
 
                     # REQUIRED: create x and y axes, set to auto scale.
-                    x_axis = dpg.add_plot_axis(dpg.mvXAxis, label='x', tag='x_axis_y')
-                    y_axis = dpg.add_plot_axis(dpg.mvYAxis, label='y', tag='y_axis_y')
+                    x_axis = dpg.add_plot_axis(dpg.mvXAxis,
+                                               label='x',
+                                               tag='x_axis_y')
+                    y_axis = dpg.add_plot_axis(dpg.mvYAxis,
+                                               label='y',
+                                               tag='y_axis_y')
 
-
-                    # series belong to a y axis. Note the tag name is used in the update
+                    # series belong to a y axis.
+                    # Note the tag name is used in the update
                     # function update_data
-                    dpg.add_line_series(x=list(self.data_x),y=list(self.accel_data_y), 
-                                        label='Accel data', parent='y_axis_y', 
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.accel_data_y),
+                                        label='Accel data',
+                                        parent='y_axis_y',
                                         tag='accel_y')
-                    
-                    dpg.add_line_series(x=list(self.data_x),y=list(self.gyro_data_y), 
-                                        label='Gyro data', parent='y_axis_y', 
+
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.gyro_data_y),
+                                        label='Gyro data',
+                                        parent='y_axis_y',
                                         tag='gyro_y')
-                    
-                    dpg.add_line_series(x=list(self.data_x),y=list(self.PID_data_y), 
-                                        label='PID data', parent='y_axis_y', 
+
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.PID_data_y),
+                                        label='PID data',
+                                        parent='y_axis_y',
                                         tag='pid_y')
-            
+
                 with dpg.plot(label='PID Z'):
-                        # optionally create legend
-                        dpg.add_plot_legend()
+                    # optionally create legend
+                    dpg.add_plot_legend()
 
-                        # REQUIRED: create x and y axes, set to auto scale.
-                        x_axis = dpg.add_plot_axis(dpg.mvXAxis, label='x', tag='x_axis_z')
-                        y_axis = dpg.add_plot_axis(dpg.mvYAxis, label='y', tag='y_axis_z')
+                    # REQUIRED: create x and y axes, set to auto scale.
+                    x_axis = dpg.add_plot_axis(dpg.mvXAxis,
+                                               label='x',
+                                               tag='x_axis_z')
+                    y_axis = dpg.add_plot_axis(dpg.mvYAxis,
+                                               label='y',
+                                               tag='y_axis_z')
 
+                    # series belong to a y axis.
+                    # Note the tag name is used in the update
+                    # function update_data
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.accel_data_z),
+                                        label='Accel data',
+                                        parent='y_axis_z',
+                                        tag='accel_z')
 
-                        # series belong to a y axis. Note the tag name is used in the update
-                        # function update_data
-                        dpg.add_line_series(x=list(self.data_x),y=list(self.accel_data_z), 
-                                            label='Accel data', parent='y_axis_z', 
-                                            tag='accel_z')
-                        
-                        dpg.add_line_series(x=list(self.data_x),y=list(self.gyro_data_z), 
-                                            label='Gyro data', parent='y_axis_z', 
-                                            tag='gyro_z')
-                        
-                        dpg.add_line_series(x=list(self.data_x),y=list(self.PID_data_z), 
-                                            label='PID data', parent='y_axis_z', 
-                                            tag='pid_z')
-                                    
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.gyro_data_z),
+                                        label='Gyro data',
+                                        parent='y_axis_z',
+                                        tag='gyro_z')
+
+                    dpg.add_line_series(x=list(self.data_x),
+                                        y=list(self.PID_data_z),
+                                        label='PID data',
+                                        parent='y_axis_z',
+                                        tag='pid_z')
+
             self.header_sim = dpg.add_text("Simulation menu:")
 
             with dpg.group(horizontal=True):
@@ -190,23 +224,30 @@ class MainApp:
             with dpg.group(horizontal=True, width=300):
                 dpg.add_text("Setpoint Z")
                 self.input_setpoint_z = dpg.add_input_text(default_value="0")
-            dpg.add_button(label="Send new Setpoint", callback=self.send_new_setpoint)
+
+            dpg.add_button(label="Send new Setpoint",
+                           callback=self.send_new_setpoint)
 
             self.med_header_limit = dpg.add_text("Limits:")
             with dpg.group(horizontal=True, width=300):
                 dpg.add_text("Motor output limit min")
-                self.input_limit_motor_min = dpg.add_input_text(default_value=str(self.limit_motor_min))
+                self.input_limit_motor_min = dpg.add_input_text(
+                                    default_value=str(self.limit_motor_min))
             with dpg.group(horizontal=True, width=300):
                 dpg.add_text("Motor output limit max")
                 self.input_limit_motor_max = dpg.add_input_text(default_value=str(self.limit_motor_max))
             dpg.add_button(label="Send new limits", callback=self.send_new_limit)
 
-
-
             #
             # File dialog
             #
-            with dpg.file_dialog(directory_selector=False, show=False, callback=self.dialog_callback, cancel_callback=self.cancel_dialog_callback, tag="file_dialog", width=700 ,height=400):
+            with dpg.file_dialog(directory_selector=False,
+                                 show=False,
+                                 callback=self.dialog_callback,
+                                 cancel_callback=self.cancel_dialog_callback,
+                                 tag="file_dialog",
+                                 width=700,
+                                 height=400):
                 dpg.add_file_extension(".*")
                 dpg.add_file_extension(".yml", color=(0, 255, 0, 255))
 
@@ -217,27 +258,27 @@ class MainApp:
                 dpg.add_button(label="Load config.yml", callback=lambda: dpg.show_item("file_dialog"))
                 dpg.add_button(label="Connect to server", callback=self.connect_to_server)
                 dpg.add_button(label="Disconnect from server", callback=self.disconnect_from_server)
-            
+
             self.server_status = dpg.add_text("Not connected to server")
 
             self.header_command_log = dpg.add_text("App logs:")
             self.log_field = dpg.add_input_text(width=800, height=200, multiline=True, readonly=True)
 
-            #bind header font
+            # bind header font
             dpg.bind_item_font(self.header_app, self.header_font)
             dpg.bind_item_font(self.header_drone, self.header_font)
             dpg.bind_item_font(self.header_sim, self.header_font)
             dpg.bind_item_font(self.header_command_log, self.header_font)
 
-            #bind medium header font
+            # bind medium header font
             dpg.bind_item_font(self.med_header_setpoint, self.med_header_font)
             dpg.bind_item_font(self.med_header_pid, self.med_header_font)
             dpg.bind_item_font(self.med_header_limit, self.med_header_font)
 
-            #bind default font
+            # bind default font
             dpg.bind_font(self.default_font)
 
-    #file dialog
+    # file dialog
     def dialog_callback(sender, app_data, user_data):
         selected_path = list(user_data["selections"].values())[0]
         contents = None
@@ -247,7 +288,7 @@ class MainApp:
             except yaml.YAMLError as exc:
                 print(exc)
                 return
-        
+
         host = contents["host"]
         port = contents["port"]
 
@@ -265,7 +306,7 @@ class MainApp:
 
         self.current_log_text += f"[{formatted_time}]: {text}\n"
         dpg.set_value(self.log_field, self.current_log_text)
-    
+
     def send_to_socket_server(self, message):
         if self.socket_server:
             self.socket_server.send(message.encode("utf-8"))
@@ -276,44 +317,51 @@ class MainApp:
         self.iter = 0
         while True:
             if self.running and self.socket_server:
-                #Getting data from sensors
+                # Getting data from sensors
                 data = self.socket_server.recv(1024).decode()
                 data_arr = data.split(",")
-                
+
                 try:
                     y_accel = 0
                     y_gyro = float(data_arr[0])
                     y_PID = float(data_arr[1])
                 except ValueError:
                     continue
-                
+
                 self.data_x.append(self.iter)
 
-                #update PID X
+                # update PID X
                 self.setpoint_x.append(self.current_setpoint_x)
                 self.accel_data_x.append(y_accel)
                 self.gyro_data_x.append(y_gyro)
                 self.PID_data_x.append(y_PID)
-                
-                #update PID Y
+
+                # update PID Y
                 self.setpoint_y.append(self.current_setpoint_y)
 
-                #update PID Z
+                # update PID Z
                 self.setpoint_z.append(self.current_setpoint_z)
 
-                #set values for PID X (TODO, add more values)
-                dpg.set_value('accel_x', [self.data_x[-self.visible_data_patch:], self.accel_data_x[-self.visible_data_patch:]])
-                dpg.set_value('gyro_x', [self.data_x[-self.visible_data_patch:], self.gyro_data_x[-self.visible_data_patch:]])
-                dpg.set_value('pid_x', [self.data_x[-self.visible_data_patch:], self.PID_data_x[-self.visible_data_patch:]])
-                dpg.set_value('setpoint_x', [self.data_x[-self.visible_data_patch:], self.setpoint_x[-self.visible_data_patch:]])
+                # set values for PID X (TODO, add more values)
+                dpg.set_value('accel_x', [
+                    self.data_x[-self.visible_data_patch:],
+                    self.accel_data_x[-self.visible_data_patch:]])
+                dpg.set_value('gyro_x', [
+                    self.data_x[-self.visible_data_patch:],
+                    self.gyro_data_x[-self.visible_data_patch:]])
+                dpg.set_value('pid_x', [
+                    self.data_x[-self.visible_data_patch:],
+                    self.PID_data_x[-self.visible_data_patch:]])
+                dpg.set_value('setpoint_x', [
+                    self.data_x[-self.visible_data_patch:],
+                    self.setpoint_x[-self.visible_data_patch:]])
 
                 dpg.fit_axis_data('x_axis_x')
                 dpg.fit_axis_data('y_axis_x')
-                
+
                 time.sleep(self.time_step)
                 self.iter += 1
 
-    
     #
     # App handlers (etc.)
     #
@@ -357,7 +405,7 @@ class MainApp:
             return
 
         self.send_to_socket_server(f"set {value_setpoint_x}")
-        #TODO send more setpoints when Andri says so
+        # TODO send more setpoints when Andri says so
         self.log("client: Sent new Setpoint data")
 
     def send_new_limit(self):
@@ -388,13 +436,13 @@ class MainApp:
 
     def app_exit(self):
         exit(0)
-    
+
     def connect_to_server(self):
         if not self.connected_to_server:
             self.host = dpg.get_value(self.input_addr)
             self.port = int(dpg.get_value(self.input_port))
 
-            #connect to server
+            # connect to server
             self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
                 self.socket_server.connect((self.host, self.port))
@@ -408,7 +456,6 @@ class MainApp:
         else:
             self.log("client: Already connected to the server")
 
-    
     def disconnect_from_server(self):
         if self.connected_to_server:
             self.socket_server.close()
@@ -420,11 +467,13 @@ class MainApp:
         else:
             self.log("client: Not connected to the server")
 
+
 def run_app():
     app = MainApp(visible_data_patch=100)
     app.gui_init()
     app.app_init()
     dpg.destroy_context()
+
 
 if __name__ == "__main__":
     run_app()
